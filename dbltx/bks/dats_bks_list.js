33c4daf77db17ee1d6797b909e7ebdf7803892ff
elio-info <dario.andreu@gmail.com>
2023-11-29 19:59:48 -0500

    tremendo relajo en libros

---
+++ dbltx/bks/dats_bks_list.js
@@ -0,0 +1,9 @@
+let books_INCA_proy= [
+    { 
+        autor:"Pedro",
+        name:" Michel Ruiz S&aacute;nchez",
+        titulo:"Dr.C. Agr&oacute;nomas Ingeniero Agr&oacute;nomo",
+        year:"2022"
+    }
+    
+]
